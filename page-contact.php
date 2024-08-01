67a2058a9fcfd7497f608c5f191665108b701fb4
YusafS94 <yusafsaddiq@gmail.com>
2024-08-01 17:32:16 +0100

    contact form fixed

--- page-contact.php
+++ page-contact.php
@@ -10,42 +10,42 @@
 
   <div class="row">
     <div class="col-md-4 order-md-2 mb-4 text-dark">
-      <h4 class="d-flex justify-content-between align-items-center mb-3">
-        <span class="">Details</span>
-        <span class="badge badge-secondary badge-pill">3</span>
+      <h4 class="d-flex justify-content-between align-items-center mb-3 text-light">
+        <span class="">Or drop us a message</span>
+        <span class="badge badge-secondary badge-pill">...</span>
       </h4>
       <ul class="list-group mb-3">
         <li class="list-group-item d-flex justify-content-between lh-condensed">
           <div>
-            <h6 class="my-0">Product name</h6>
-            <small class="text-muted">Brief description</small>
+            <h6 class="my-0">Name</h6>
+            <small class="text-muted">Kierhardy Ansell</small>
           </div>
-          <span class="text-muted">$12</span>
+          <span class="text-muted">...</span>
         </li>
         <li class="list-group-item d-flex justify-content-between lh-condensed">
           <div>
-            <h6 class="my-0">Second product</h6>
-            <small class="text-muted">Brief description</small>
+            <h6 class="my-0">Email</h6>
+            <small class="text-muted">Email@gmail.com</small>
           </div>
-          <span class="text-muted">$8</span>
+          <span class="text-muted">...</span>
         </li>
         <li class="list-group-item d-flex justify-content-between lh-condensed">
           <div>
-            <h6 class="my-0">Third item</h6>
-            <small class="text-muted">Brief description</small>
+            <h6 class="my-0">Phone</h6>
+            <small class="text-muted">01924 000111</small>
           </div>
-          <span class="text-muted">$5</span>
+          <span class="text-muted">...</span>
         </li>
         <li class="list-group-item d-flex justify-content-between bg-light">
           <div class="text-success">
-            <h6 class="my-0">Promo code</h6>
-            <small>EXAMPLECODE</small>
+            <h6 class="my-0">Address</h6>
+            <small>123 Brick Lane</small>
           </div>
-          <span class="text-success">-$5</span>
+          <span class="text-success">...</span>
         </li>
         <li class="list-group-item d-flex justify-content-between">
-          <span>Total (USD)</span>
-          <strong>$20</strong>
+          <span>Hours</span>
+          <strong>...</strong>
         </li>
       </ul>
 
@@ -70,46 +70,17 @@
           </div>
         </div>
 
-        
-
-        
-
-        
-
-        
 
         <div class="row">
-          <div class="col-md-5 mb-3">
-            <label for="country">Country</label>
-            <select class="custom-select d-block w-100" id="country" required="">
-              <option value="">Choose...</option>
-              <option>United States</option>
-            </select>
-            <div class="invalid-feedback">
-              Please select a valid country.
-            </div>
-          </div>
-          <div class="col-md-4 mb-3">
-            <label for="state">State</label>
-            <select class="custom-select d-block w-100" id="state" required="">
-              <option value="">Choose...</option>
-              <option>California</option>
-            </select>
-            <div class="invalid-feedback">
-              Please provide a valid state.
-            </div>
-          </div>
-          <div class="col-md-3 mb-3">
-            <label for="zip">Zip</label>
-            <input type="text" class="form-control" id="zip" placeholder="" required="">
-            <div class="invalid-feedback">
-              Zip code required.
-            </div>
+          <div class="col-md-12 mb-3">
+            <label for="message">Message:</label>
+            <textarea class="form-control" id="message" name="message" rows="4" cols="50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ex corrupti necessitatibus exercitationem in est reprehenderit mollitia laboriosam repellendus molestiae.
+            </textarea>
           </div>
         </div>
         
         <hr class="mb-4">
-        <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+        <button class="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
       </form>
     </div>
   </div>
