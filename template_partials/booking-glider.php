6b9ad878d9804049993fa6d4121b8ade355a00d4
YusafS94 <yusafsaddiq@gmail.com>
2024-08-16 17:31:04 +0100

    card check booking

--- template_partials/booking-glider.php
+++ template_partials/booking-glider.php
@@ -1,6 +1,25 @@
 <div class="glide container">
     <div class="glide__track" data-glide-el="track">
       <ul class="glide__slides">
+      <li class="glide__slide">
+            <!-- Card test -->
+          <div class="custom-card bg-light">
+            <img class="card-image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
+            <div class="card-rounded-container">
+              <h3 class="container border-bottom border-primary p-2 m-0 text-uppercase text-left">Halifax</h3>
+              <div class="row m-0">
+                <div class="col h-100 border-right border-primary">
+                  <p class="">A group skateboard lesson open to all abilities! Hosted at the Wellholme Skatepark. HD6 4AA (Brighouse).</p>
+                  <p class="">Weather permitting - Alternative session hosted at Orange Box Skatepark if session is cancelled.</p>
+                </div>
+                <div class="w-25">
+                  <p class="container text-center border-bottom border-primary w-100">1 hour @ £12.50</p>
+                  <img class="mid-star w-100 h-50 text-center" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+
         <li class="glide__slide">
             <!-- Card 1 -->
             <img class="about-img border border-dark booking-card-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
