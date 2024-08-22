8cdaa0504bb7131c454d882881d241f83cae350c
YusafS94 <yusafsaddiq@gmail.com>
2024-08-22 17:33:30 +0100

    spacing, scroll text

--- front-page.php
+++ front-page.php
@@ -9,46 +9,36 @@
       </div>
   </section>
 
- 
-
   <section>
     <div class="container opac">
       <div class="title-bg">
         <h1 class="title-large text-main_purple">Skateboarding <br> in Yorkshire</h1>
       </div>
       <div class="row container border border-dark mx-auto">
-        <div class="col border-right border-dark">
+        <div class="col about-left-col">
           <div class="stroke-main_purple">
             <h2 class="title-medium text-light">Award Winning</h2>
             <h3 class="title-small text-light">Skateboarding</h3>
             <h2 class="title-medium text-light">Company</h2>
           </div>
-          <div class="row border-top border-bottom border-dark title-xxs text-main_purple scroll-outer">
-            <div class="scroll-inner d-flex align-items-center">
-              <p class="d-inline">More than a club </p>
-              <img class="small-star" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt="">
-              <p>More than a club</p>
-              <img class="small-star" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt="">
-              <p>More than a club</p>
-            </div>
-          </div>
-          <div class="border border-dark text-center my-2 bg-light rounded-xl p-4">
+          <?php include_partial("scrolling-text"); ?>
+          <div class="border border-dark text-center my-2 bg-light rounded-xl">
             <h3 class="border-bottom border-dark title-xs">Our mission</h3>
             <p>We aim to be more than a club but instead a community of skateboarders!</p>
             <p>By joining us you'll be introduced to the whole of skateboarding and make new friends along the way!</p>
           </div>
         </div>
         <div class="col">
-          <div class="top bg-light rounded-xl p-4 m-4 border-main_purple ">
-            <h3 class="border-bottom border-dark title-xs">About us</h3>
-            <div class="row">
-              <div class="col border-right border-dark"><img class="about-star" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt=""></div>
-              <div class="col">
+          <div class="top bg-light rounded-xl border-main_purple ">
+            <h3 class="border-bottom border-dark title-xs m-0">About us</h3>
+            <div class="d-flex">
+              <div class="about-star-container"><img class="about-star" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt=""></div>
+              <div class="">
                 <p>We run and operate the Orange Box skatepark <strong>based in the heart of Halifax</strong></p>
               </div>
             </div>
           </div>
-          <div class="bottom">
+          <div class="bottom m-2">
             <img class="about-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
           </div>
         </div>
