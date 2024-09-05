e68be71142c4d9f0d4bd08a8f7b23844f216c1e0
YusafS94 <yusafsaddiq@gmail.com>
2024-09-05 21:21:42 +0100

    join on the fun, nav header img, carousel

--- page-halifax.php
+++ page-halifax.php
@@ -1,31 +1,38 @@
 <?php get_header(); ?>
 
 <main class="main">
-    <div class="container">
-                <div class="bg-light rounded-xl border-main d-flex flex-column flex-md-row">
-                    <div class="booking-value-prop-img w-100"><img class="book-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt=""></div>
-                    <div class="w-25 m-auto"><img class="book-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt=""></div>
-                    <div class="w-100">
-                        <h3 class="px-5 pt-3">Free equipment provided at all sessions</h3>
-                        <p class="px-5 pt-3">To save your spot and sign up to a lesson weekly <strong>send us a message</strong> or for a 1-1 purchase a package through the link below</p>
-                        <a href="/" class="btn btn-warning rounded-pill mx-5 mt-3 py-2 px-4">View Products/Packages <i class="fa fa-arrow-right"></i></a>
-                    </div>
+    <section class="checker-bg py-5">
+        <div class="container">
+            <div class="bg-purple rounded-xl border-main d-flex flex-column flex-md-row">
+                <div class="booking-value-prop-img w-100"><img class="book-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt=""></div>
+                <div class="w-25 m-auto"><img class="book-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dark/yellow-star.svg" alt=""></div>
+                <div class="w-100">
+                    <h3 class="px-5 pt-3 text-yellow">Free equipment provided at all sessions</h3>
+                    <p class="px-5 pt-3 text-yellow">To save your spot and sign up to a lesson weekly <strong>send us a message</strong> or for a 1-1 purchase a package through the link below</p>
+                    <a href="#halifax-glider-section" class="btn btn-warning rounded-pill mx-5 mt-3 py-2 px-4">View Products/Packages <i class="fa fa-arrow-right"></i></a>
                 </div>
-    </div>
+            </div>
+        </div>
+    </section>
 
     
 
 
-    <section class="paper-bg p-4">
+    <section id="halifax-glider-section" class="paper-bg p-4">
         <div class="my-4">
             <?php include_partial("halifax-booking-glider"); ?>
         </div>
     </section>
 
-    <section>
-        <div class="container text-center">
-            <h1 class="title-large text-main">Join on the fun</h1>
-            <h3 class="text-main">Follow us on our socials:</h3>
+    <section class="py-5">
+        <div class="container text-center opac">
+            <h1 class="title-medium text-yellow">Join on the fun</h1>
+            <h3 class="text-yellow">Follow us on our socials:</h3>
+            <div class="d-flex justify-content-center">
+                <a class="social-icons" target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dark/ig-yellow.svg" alt=""></a>
+                <a class="social-icons" target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dark/fb-yellow.svg" alt=""></a>
+                <a class="social-icons" target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dark/twitter-yellow.svg" alt=""></a>
+            </div>
         </div>
     </section>
 
--- template_partials/halifax-booking-glider.php
+++ template_partials/halifax-booking-glider.php
@@ -1,16 +1,16 @@
-<div class="glide">
+<div class="glide container">
     <div class="glide__track" data-glide-el="track">
       <ul class="glide__slides">
 
         <li class="glide__slide">
             <!-- Card 1 -->
-            <img class="about-img border border-dark booking-card-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
+            <img class="about-img border-purple booking-card-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
             <div class="">
                 <div class="d-flex flex-column align-items-center text-center">
-                    <p>All</p>
-                    <h4>View all club packages</h4>
-                    <p>View full range of packages available at the Orange Box (Orange Box Skatepark, HX1 1AF). Open to customers and club members.</p>
-                    <p>All equipment provided for free!</p>
+                    <p class="text-purple">All</p>
+                    <h4 class="text-purple">View all club packages</h4>
+                    <p class="text-purple">View full range of packages available at the Orange Box (Orange Box Skatepark, HX1 1AF). Open to customers and club members.</p>
+                    <p class="text-purple">All equipment provided for free!</p>
                     <a target="blank" href="https://siybooking.as.me/?appointmentType=category:Orange%20Box" class="btn btn-warning rounded-pill py-2 px-4">Book</a>
                 </div>
             </div>
@@ -18,13 +18,13 @@
 
         <li class="glide__slide">
             <!-- Card 2 -->
-            <img class="about-img border border-dark" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
+            <img class="about-img border-purple" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
             <div class="">
                 <div class="d-flex flex-column align-items-center text-center">
-                    <p>45 minutes @ £30.00</p>
-                    <h4>1-1 private session</h4>
-                    <p>45 minute private lesson with expert coaches and access to private skatepark open to 1-1 customers (Orange Box Skatepark, HX1 1AF).</p>
-                    <p>All equipment provided for free!</p>
+                    <p class="text-purple">45 minutes @ £30.00</p>
+                    <h4 class="text-purple">1-1 private session</h4>
+                    <p class="text-purple">45 minute private lesson with expert coaches and access to private skatepark open to 1-1 customers (Orange Box Skatepark, HX1 1AF).</p>
+                    <p class="text-purple">All equipment provided for free!</p>
                     <a target="blank" href="https://siybooking.as.me/PrivateLesson" class="btn btn-warning rounded-pill py-2 px-4">Book</a>
                 </div>
             </div>
@@ -32,13 +32,13 @@
 
         <li class="glide__slide">
             <!-- Card 3 -->
-            <img class="about-img border border-dark" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
+            <img class="about-img border-purple" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/2.jpg" alt="">
             <div class="">
                 <div class="d-flex flex-column align-items-center text-center">
-                    <p>2 hours @ £3.00</p>
-                    <h4>2 hour public access</h4>
-                    <p>2 hour public access to the skate park (Orange Box Skatepark, HX1 1AF).</p>
-                    <p>All equipment provided for free!</p>
+                    <p class="text-purple">2 hours @ £3.00</p>
+                    <h4 class="text-purple">2 hour public access</h4>
+                    <p class="text-purple">2 hour public access to the skate park (Orange Box Skatepark, HX1 1AF).</p>
+                    <p class="text-purple">All equipment provided for free!</p>
                     <a target="blank" href="https://siybooking.as.me/OrangeBox" class="btn btn-warning rounded-pill py-2 px-4">Book</a>
                 </div>
             </div>
