a88593e7d42e16eb6e6a846abf3246b98f247b3d
YusafS94 <yusafsaddiq@gmail.com>
2024-10-04 21:07:07 +0100

    all glider acf changes finished

--- template_partials/landing-glider.php
+++ template_partials/landing-glider.php
@@ -1,3 +1,8 @@
+<?php 
+$landingCardImage1 = get_field('landing_card_1_img');
+$landingCardImage2 = get_field('landing_card_2_img');
+$landingCardImage3 = get_field('landing_card_3_img');
+?>
 <div class="glide container">
     <div class="glide__track" data-glide-el="track">
       <ul class="glide__slides">
@@ -6,14 +11,14 @@
             <!-- Card 1 -->
           <div class="custom-card bg-yellow">
             <div class="card-div w-100 border border-dark">
-              <img class="card-image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dark/halifax-card-resized.jpg" alt="">
+              <img class="card-image" src="<?php echo esc_url($landingCardImage1['url']); ?>" alt="<?php echo esc_attr($landingCardImage1['alt']); ?>">
             </div>
             <div class="card-rounded-container w-100">
               <h3 class="container border-bottom border-primary p-2 m-0 text-uppercase text-center text-purple">Halifax</h3>
               <div class="d-flex">
                 <div class="w-100 border-right border-primary px-4">
-                  <p class="text-purple">Visit our private rooftop skatepark in the heart of Halifax.</p>
-                  <p class="text-purple">Located in the Orange Box @ 1 Blackledge, HX1 1AF.</p>
+                  <p class="text-purple"><?php the_field('landing_card_1_description_1'); ?></p>
+                  <p class="text-purple"><?php the_field('landing_card_1_description_2'); ?></p>
                 </div>
                 <div class="w-50">
                   <img class="mid-star w-100 text-center my-5" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt="">
@@ -28,14 +33,14 @@
             <!-- Card 2 -->
           <div class="custom-card bg-yellow">
             <div class="card-div w-100 border border-dark">
-              <img class="card-image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dark/huddersfield-card-resized.jpg" alt="">
+            <img class="card-image" src="<?php echo esc_url($landingCardImage2['url']); ?>" alt="<?php echo esc_attr($landingCardImage2['alt']); ?>">
             </div>
             <div class="card-rounded-container w-100">
               <h3 class="container border-bottom border-primary p-2 m-0 text-uppercase text-center text-purple">Huddersfield</h3>
               <div class="d-flex">
                 <div class="w-100 border-right border-primary px-4">
-                  <p class="text-purple">Visit our private skatepark located in an old toyshop!</p>
-                  <p class="text-purple">Find us @ The Piazza Centre, HD1 2RS.</p>
+                  <p class="text-purple"><?php the_field('landing_card_2_description_1'); ?></p>
+                  <p class="text-purple"><?php the_field('landing_card_2_description_2'); ?></p>
                 </div>
                 <div class="w-50">
                   <img class="mid-star w-100 text-center my-5" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt="">
@@ -50,13 +55,14 @@
             <!-- Card 3 -->
           <div class="custom-card bg-yellow">
             <div class="card-div w-100 border border-dark">
-              <img class="card-image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dark/castleford-card-resized.jpg" alt="">
+            <img class="card-image" src="<?php echo esc_url($landingCardImage3['url']); ?>" alt="<?php echo esc_attr($landingCardImage3['alt']); ?>">
             </div>
             <div class="card-rounded-container w-100">
               <h3 class="container border-bottom border-primary p-2 m-0 text-uppercase text-center text-purple">Castleford</h3>
               <div class="d-flex">
                 <div class="w-100 border-right border-primary px-4">
-                  <p class="text-purple">Coming soon to a Castleford near you...</p>
+                  <p class="text-purple"><?php the_field('landing_card_3_description_1'); ?></p>
+                  <p class="text-purple"><?php the_field('landing_card_3_description_2'); ?></p>
                 </div>
                 <div class="w-50">
                   <img class="mid-star w-100 text-center my-5" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/STAR-03.svg" alt="">
